295261801cc2300e2717b862855387e3a4a550fb
Strahil Peykov <strahil.peykov@gmail.com>
2022-10-15 16:15:53 +0200

    manual cases

--- sketch_oct15a/sketch_oct15a.ino
+++ sketch_oct15a/sketch_oct15a.ino
@@ -24,6 +24,11 @@ float tempOutside;
 
 int val_analogique;
 
+bool isSpinning=false; //servo is spinning
+bool isAutomatic=true; //automated process is running (Determined by a switch)
+bool isWindowOpen=false; //window is closed or open
+bool isSwitchOpen=false; // switch for closing and opening window manually
+
 void setup()
 {
   pinMode(capteur_D, INPUT);
@@ -32,13 +37,12 @@ void setup()
   dhtInside.begin();
   dhtOutside.begin();
 
-  pinMode(8, INPUT);
+  pinMode(11, INPUT);
 
   //myservo.write(pos); // put it before the attach() so it goes straight to that position
   //myservo.writeMicroseconds(1300); 
 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-
 
 
   /*
@@ -52,7 +56,44 @@ void setup()
   */
 }
 
-void loop()
+void loop(){
+  //isAutomatic=digitalRead(); //TBD
+  isAutomatic=true;
+  if(isAutomatic==false){
+    //isSwitchOn=digitalRead(); //TBD
+   
+    if(isSwitchOpen==true&&isWindowOpen==false){
+      OpenWindow();
+    }
+    else if(isSwitchOpen==false && isWindowOpen==true){
+      CloseWindow();
+      isWindowOpen=false;
+    }
+  }
+  else{
+    test();
+  }
+}
+
+void OpenWindow(){
+  myservo.write(0);
+  delay(5000);
+  myservo.write(90);
+}
+
+void CloseWindow(){
+  myservo.write(180);
+  delay(100);
+  while(digitalRead(11)==LOW){
+    delay(10);
+  }            
+
+  myservo.write(90);
+  
+}
+
+
+void test()
 {
 
   humInside = dhtInside.readHumidity();
@@ -136,4 +177,3 @@ void loop()
    
 
 }
-s
