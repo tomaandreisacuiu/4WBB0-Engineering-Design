ba51b07c422d2a14d0a9f82a29d922ac2f4a777f
Strahil Peykov <strahil.peykov@gmail.com>
2022-10-15 16:20:23 +0200

    removed code

--- sketch_oct15a/sketch_oct15a.ino
+++ sketch_oct15a/sketch_oct15a.ino
@@ -139,25 +139,6 @@ void test()
   delay(1000);
 
 
-  /*
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-    }
-
-
-    /*
-    delay(3000);
-    myservo.write(90);
-    delay(3000);
-    myservo.write(180);
-    delay(3000);
-  */
 
 
   /*
@@ -175,12 +156,6 @@ void test()
       }
   */
 
-  //myservo.write(180);
-
-
-  if (digitalRead(8) == HIGH) {
-    myservo.write(90);
-  }
 
 
 }
