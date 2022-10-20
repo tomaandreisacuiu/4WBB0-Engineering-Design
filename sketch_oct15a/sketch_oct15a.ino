90153fb0592c5169af83c2c12a977011b1a28ed5
Strahil Peykov <strahil.peykov@gmail.com>
2022-10-20 19:26:31 +0200

    lcd

--- sketch_oct15a/sketch_oct15a.ino
+++ sketch_oct15a/sketch_oct15a.ino
@@ -1,5 +1,9 @@
 #include <DHT.h>
 #include <Servo.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 // set the DHT Pins
 #define DHTPIN 7
@@ -15,13 +19,13 @@ Servo myservo;
 
 const int rain_sensor_D = 2; // Rain Sensor Connection D0
 const int rain_sensor_A = A0; // Rain Sensor Connection A0
-const int servo_D = 9;
-const int magnet_D = 4;
-const int switch_auto_D = 5;
-const int switch_open_D = 6;
-const int switch_adjust_D=7;
-const int button_temp1 = 8;
-const int button_temp2 = 10;
+const int servo_D = 9; // servo motor
+const int magnet_D = 4; // magnet window sensor (for closing)
+const int switch_auto_D = 5; // switch for automation
+const int switch_open_D = 6; // switch for opening/closing window in manual mode
+const int switch_adjust_D = 11; // switch to enter "Adjust Temperature" mode
+const int button_temp1 = 12; // button to decrease preffered temperature
+const int button_temp2 = 13; // button to increase preffered temperature
 
 //Variables
 float humInside;  //Humidity Inside
@@ -32,7 +36,7 @@ float dewInside; // Dew Point Inside
 float dewOutside; // Dew Point Outside
 int rainAnalog; // Analog Value of Rain Sensor
 
-float prefferedTemp;
+float prefferedTemp = 20; // preferred inside temperature
 
 bool isSpinning = false; //servo is spinning
 bool isAutomatic = true; //automated process is running (Determined by a switch)
@@ -53,33 +57,49 @@ void setup()
   pinMode(button_temp2, INPUT_PULLUP);
 
 
+
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
   delay(500);//Delay to let system boot
 
   dhtInside.begin();
   dhtOutside.begin();
-
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-
   delay(1000);//Wait before accessing Sensor
-  isAutomatic = false;
-  isSwitchOpen = false;
-  isWindowOpen = true;
+
+  lcd.init(); // initialize the LCD
+  lcd.backlight(); // enable backlight of LCD
 
 
 }
 
 void loop() {
+  lcd.setCursor(15, 0);
+  lcd.print("18.0C");
 
+  lcd.setCursor(0, 2);
+  lcd.print("Hum :");
+  lcd.setCursor(8, 2);
+  lcd.print("18.0%");
+  lcd.setCursor(15, 2);
+  lcd.print("18.0%");
+
+  lcd.setCursor(0, 3);
+  lcd.print("Temp:");
+  lcd.setCursor(8, 3);
+  lcd.print("18.0C");
+  lcd.setCursor(15, 3);
+  lcd.print("18.0C");
   isAutomatic = (!digitalRead(switch_auto_D));
   isAdjustTemp = (!digitalRead(switch_adjust_D));
   if (isAdjustTemp == true) {
-    prefferedTemp=AdjustTemp(prefferedTemp);
+    prefferedTemp = AdjustTemp(prefferedTemp);
   }
   else {
 
     if (isAutomatic == false) {
-      isSwitchOpen = digitalRead(switch_open_D);
+      lcd.setCursor(0, 0);
+      lcd.print("Manual Mode   ");
+      isSwitchOpen = (!digitalRead(switch_open_D));
       if (isSwitchOpen == true) {
         OpenWindow();
       }
@@ -88,6 +108,8 @@ void loop() {
       }
     }
     else {
+      lcd.setCursor(0, 0);
+      lcd.print("Automatic Mode");
       test();
     }
     delay(1000); //Wait 3 seconds before accessing sensor again.
@@ -97,9 +119,14 @@ void loop() {
 
 void OpenWindow() {
   if (isWindowOpen == false) {
+    lcd.setCursor(0, 1);
+    lcd.print("Opening");
     myservo.write(0); // start rotating to open window full-speed
     delay(3000);// takes 3 seconds to open window
     myservo.write(90); //stop rotating
+    lcd.setCursor(0, 1);
+    lcd.print("       ");
+
     isWindowOpen = true;
   }
 
@@ -107,6 +134,8 @@ void OpenWindow() {
 
 void CloseWindow() {
   if (isWindowOpen == true) {
+    lcd.setCursor(0, 1);
+    lcd.print("Closing");
     myservo.write(180); // start rotating to close window full-speed
     delay(100);
 
@@ -115,25 +144,34 @@ void CloseWindow() {
     }
 
     myservo.write(90); // stop rotating
+
+    lcd.setCursor(0, 1);
+    lcd.print("       ");
     isWindowOpen = false;
 
 
   }
 }
 
-float AdjustTemp(float temp){
-    isAdjustTemp = (!digitalRead(switch_adjust_D));
-  while(isAdjustTemp==true){
-    if(!digitalRead(button_temp1)==HIGH){
-      temp-=0.5;
+float AdjustTemp(float temp) {
+  isAdjustTemp = (!digitalRead(switch_adjust_D));
+  lcd.setCursor(0, 0);
+  lcd.print("Adjust Mode");
+
+  while (isAdjustTemp == true) {
+    lcd.setCursor(0, 1);
+    lcd.print(temp);
+    if (!digitalRead(button_temp1) == HIGH) {
+      temp -= 0.5;
       delay(500);
     }
-    if(!digitalRead(button_temp2)==HIGH){
-      temp+=0.5;
+    if (!digitalRead(button_temp2) == HIGH) {
+      temp += 0.5;
       delay(500);
     }
     return temp;
   }
+
 }
 
 
@@ -157,6 +195,23 @@ void test()
   Serial.println(tempInside);
   Serial.println(tempOutside);
   Serial.println();
+
+  lcd.setCursor(15, 0);
+  lcd.print("18.0C");
+
+  lcd.setCursor(0, 2);
+  lcd.print("Hum :");
+  lcd.setCursor(8, 2);
+  lcd.print(humInside);
+  lcd.setCursor(15, 2);
+  lcd.print(humOutside);
+
+  lcd.setCursor(0, 3);
+  lcd.print("Temp:");
+  lcd.setCursor(8, 3);
+  lcd.print(tempInside);
+  lcd.setCursor(15, 3);
+  lcd.print(tempOutside);
 
 
   if (digitalRead(rain_sensor_D) == LOW)
@@ -197,8 +252,11 @@ void test()
   else {
     CloseWindow();
   }
+}
 
-
-
-
+void ClearLine(int line) {
+  for (int i = 0; i < 20; i++) {
+    lcd.setCursor(i, line);
+    lcd.print(" ");
+  }
 }
